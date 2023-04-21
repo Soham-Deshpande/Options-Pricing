3098316896926e9519aaa8e11d224d3ff8f8e742
Soham <sohamnd19@gmail.com>
2023-04-21 12:27:43 +0100

    Update ImpliedVolatility.py

--- ImpliedVolatility.py
+++ ImpliedVolatility.py
@@ -36,11 +36,11 @@ def implied_volatility(S, K, T, r, sigma, option_type, jump_size, jump_probabili
   return implied_volatility
 
 
-import math
+import numpy as np
 
-def jump_probability(S, K, T, r, sigma, option_type, jump_size, jump_probability):
+def jump_probability(S, K, T, r, sigma, option_type, historical_data):
   """
-  This function calculates the jump probability of a European option using a more realistic model for the underlying asset.
+  This function estimates the jump probability of a European option using a more realistic model for the underlying asset.
 
   Args:
     S: The current price of the underlying asset.
@@ -49,27 +49,25 @@ def jump_probability(S, K, T, r, sigma, option_type, jump_size, jump_probability
     r: The risk-free interest rate.
     sigma: The volatility of the underlying asset.
     option_type: The type of option.
-    jump_size: The size of the jumps in the underlying asset price.
-    jump_probability: The probability of a jump occurring.
+    historical_data: A list of historical prices of the underlying asset.
 
   Returns:
-    The jump probability of the option.
+    The estimated jump probability of the option.
   """
-
-  d1 = (math.log(S / K) + (r + sigma ** 2 / 2) * T) / (sigma * math.sqrt(T))
-  d2 = d1 - sigma * math.sqrt(T)
-
-  if option_type == "call":
-    call_price = S * math.normcdf(d1) - K * math.exp(-r * T) * math.normcdf(d2)
-  elif option_type == "put":
-    put_price = K * math.exp(-r * T) * math.normcdf(-d2) - S * math.normcdf(-d1)
-  else:
-    raise ValueError("Invalid option type")
 
   # Calculate the implied volatility
   implied_volatility = math.sqrt((math.log(S / K) + (r + sigma ** 2 / 2) * T) ** 2 / (2 * sigma ** 2 * T))
 
-  # Calculate the jump probability
-  jump_probability = (call_price - implied_volatility * math.sqrt(T)) / jump_size
+  # Fit the Poisson process model to the historical data
+  jumps = []
+  for i in range(len(historical_data) - 1):
+    if historical_data[i] < historical_data[i + 1]:
+      jumps.append(historical_data[i + 1] - historical_data[i])
+
+  if len(jumps) == 0:
+    return 0
+
+  # Estimate the jump probability
+  jump_probability = len(jumps) / len(historical_data)
 
   return jump_probability
